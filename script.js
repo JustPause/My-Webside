d579605f5464f7f5a568998bf848eb6d1c1d5b0a
JustPauseDebian <IamJustStan@hotmail.com>
2023-04-11 01:02:02 +0300

    A

---
+++ script.js
@@ -0,0 +1,13 @@
+let app = new Vue
+    ({
+        el: '#app',
+        data:
+        {
+            alert: "This is an alert message!",
+            project:
+                [
+                    { title: "portolio", desc: "Lor ip" },
+                    { title: "Twitter clone", desc: "Lor ip" }
+                ]
+        }
+    });
