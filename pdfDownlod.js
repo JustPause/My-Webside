7398233b9a2a5065154c4466f302cb649e41ff54
JustPause <iamjuststan@hotmail.com>
2024-04-07 18:57:47 +0300

    A

--- pdfDownlod.js
+++ pdfDownlod.js
@@ -2,7 +2,7 @@ function SavePage() {
   var element = document.getElementById("download");
   let opt = {
     margin: 0,
-    // dpi: 192,
+    dpi: 500,
     filename: 'Justinas-Stankunas-CV.pdf',
     jsPDF: { format: 'a4', orientation: 'portrait' }
   }
