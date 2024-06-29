582e3df31f941a10d913ee0011b4d1411bfa5fe0
JustPause <Justinaslolt@gmail.com>
2024-06-29 05:28:28 +0300

    upd

--- src/components/PartThree.vue
+++ src/components/PartThree.vue
@@ -1,48 +1,42 @@
 <script>
-import connectBar from './connectBar.vue'
+import connectBar from "./connectBar.vue";
 </script>
 <template>
-    <div class="text flex" style="padding-bottom: 1em;">
-        <div id="Left">
-            <h2>As a programmer, I am dedicated to creating robust and maintainable products. In other words, I strive
-                to produce works of art.</h2>
-        </div>
-        <div class="SplitH"></div>
-        <div id="NotLeft">
-            <a href="#CV">(About me)</a>
-            <connectBar />
-        </div>
+  <div class="text flex" style="padding-bottom: 1em">
+    <div id="Left">
+      <h2>
+        As a programmer, I am dedicated to creating robust and maintainable
+        products. In other words, I strive to produce works of art.
+      </h2>
     </div>
-    <div class="SplitV"></div>
+    <div class="SplitH"></div>
+    <div id="NotLeft">
+      <a href="#CV">(About me)</a>
 
+      <div id="z213a">
+        <a class="links">Linkin</a>
+        <a class="links">Github</a>
+        <a class="links">Telegram</a>
+      </div>
+
+      <connectBar />
+    </div>
+  </div>
+  <div class="SplitV"></div>
 </template>
 
 <style>
 :root {
-    --size: 0.2em;
+  --size: 0.2em;
 }
 
 #Left {
-    width: 59.65%;
-    float: left;
+  width: 59.65%;
+  float: left;
 }
 
 #NotLeft {
-    width: auto;
-    float: right;
-}
-
-#z213a {
-    /* height: 78%; */
-
-    margin-top: 20%;
-
-}
-
-.links {
-    background-color: var(--color-background);
-    border-radius: var(--border-radius);
-    padding: 0.5em 1em;
-    margin-right: 5%;
+  width: auto;
+  float: right;
 }
 </style>
--- src/components/connectBar.vue
+++ src/components/connectBar.vue
@@ -1,22 +1,25 @@
 <template>
-  <div id="z213a">
-    <a class="links">Linkin</a>
-    <a class="links">Github</a>
-    <a class="links">Telegram</a>
-  </div>
+      <div id="z213a">
+        <a class="links">Linkin</a>
+        <a class="links">Github</a>
+        <a class="links">Telegram</a>
+      </div>
 </template>
 
 <style>
 #z213a {
   /* height: 78%; */
 
+  width: 100%;
   margin-top: 20%;
 }
 
 .links {
+  display: flex
+  inline-flex;
+  text-align: left;
   background-color: var(--color-background);
   border-radius: var(--border-radius);
-  padding: 0.5em 1em;
-  margin-right: 5%;
+  padding: 0.5em;
 }
 </style>
