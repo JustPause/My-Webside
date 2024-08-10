5c8ffd48f0c3ccc25610999fba590e51c7bacdc8
JustPause <iamjuststan@hotmail.com>
2024-08-10 04:59:30 +0300

    A

--- src/components/Index.vue
+++ src/components/Index.vue
@@ -11,8 +11,18 @@
             - code tends to have a mind of its own</p>
     </div>
 
-    <div name="ProgrammingPart" className="flex content-center items-center gap-52">
-        <p>The process of  developing software is both time-consuming and incredibly rewarding for  me. It involves translating my ideas and concepts into functional fings,  you can't grasp it but you can feel it. In general, software can  significantly influence people's lives and my future programs will be  capable of affecting others. The process of software development  requires careful attention to detail and a strong enthusiasm for solving  problems, and this career path enables me to continually learn and  adjust to new technologies. I found my path in low-level languages,  where the power is in your hands, I like to feel like I can do something  special, something that other people can't do. I like to expand my  knowledge to other topics, from DevOps to the front end, reverse  engineering, and memory manipulation. That lets me get a feel for other  subjects, and I really like that feeling. of course, it kinda distracts  me from reaching the goal that I set for myself, and I see it as a good  thing, it helps me keep my interest in computers and not to burn out.</p>
+    <div name="ProgrammingPart" className="TextLayout TextStyle">
+        <p>The process of developing software is both time-consuming and incredibly rewarding for me. It involves
+            translating my ideas and concepts into functional fings, you can't grasp it but you can feel it. In general,
+            software can significantly influence people's lives and my future programs will be capable of affecting
+            others. The process of software development requires careful attention to detail and a strong enthusiasm for
+            solving problems, and this career path enables me to continually learn and adjust to new technologies. I
+            found my path in low-level languages, where the power is in your hands, I like to feel like I can do
+            something special, something that other people can't do. I like to expand my knowledge to other topics, from
+            DevOps to the front end, reverse engineering, and memory manipulation. That lets me get a feel for other
+            subjects, and I really like that feeling. of course, it kinda distracts me from reaching the goal that I set
+            for myself, and I see it as a good thing, it helps me keep my interest in computers and not to burn out.</p>
+        <div className="imigeLayout imigeStyle"></div>
     </div>
 
 </template>
@@ -24,5 +34,36 @@
 
 color {
     color: var(--color-highlighters);
+}
+
+.imigeLayout {
+    width: 43em;
+    height: 32em;
+    flex-shrink: 0;
+}
+
+.imigeStyle {
+    border-radius: 30px;
+    border: 1px #000;
+    background: url(/src/img/Me.jpg) lightgray 50% / cover no-repeat;
+    box-shadow: 0px 4px 24px 0px rgba(0, 0, 0, 0.25);
+}
+
+.TextLayout {
+    width: 520px;
+    height: 160px;
+    flex-shrink: 0;
+}
+
+.TextStyle {
+    color: #FFF;
+    padding: 3em;
+    fill: linear-gradient(90deg, rgba(255, 255, 255, 0.10) 0%, rgba(0, 0, 0, 0.10) 100%);
+
+    stroke-width: 1px;
+    stroke: #000;
+
+    filter: drop-shadow(0px 4px 24px rgba(0, 0, 0, 0.25));
+    backdrop-filter: blur(20px);
 }
 </style>
