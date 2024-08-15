57af80df20d2ce39c47f072614701f7e9c35173e
JustPause <iamjuststan@hotmail.com>
2024-08-15 16:31:35 +0300

    chec

--- src/components/Index.vue
+++ src/components/Index.vue
@@ -1,14 +1,20 @@
 <script setup>
 import TextAndImiges from "./TextAndImiges.vue"
+import TextAndImigesReverst from "./TextAndImigesReverst.vue"
 
-const text = [
-    "The process of developing software is both time-consuming and incredibly rewarding for me. It involves translating my ideas and concepts into functional fings, you can\'t grasp it but you can feel it. In general, software can significantly influence people\'s lives and my future programs will be capable of affecting others. The process of software development requires careful attention to detail and a strong enthusiasm for solving problems, and this career path enables me to continually learn and adjust to new technologies. I found my path in low-level languages, where the power is in your hands, I like to feel like I can do something special, something that other people can\'t do. I like to expand my knowledge to other topics, from DevOps to the front end, reverse engineering, and memory manipulation. That lets me get a feel for other subjects, and I really like that feeling. of course, it kinda distracts me from reaching the goal that I set for myself, and I see it as a good thing, it helps me keep my interest in computers and not to burn out.",
-
-]
-const img = [
-    "Me",
-    "3D",
-    "Sky"
+const page = [
+    {
+        text: 'The process of developing software is both time-consuming and incredibly rewarding for me. It involves translating my ideas and concepts into functional fings, you can\'t grasp it but you can feel it. In general, software can significantly influence people\'s lives and my future programs will be capable of affecting others. The process of software development requires careful attention to detail and a strong enthusiasm for solving problems, and this career path enables me to continually learn and adjust to new technologies. I found my path in low-level languages, where the power is in your hands, I like to feel like I can do something special, something that other people can\'t do. I like to expand my knowledge to other topics, from DevOps to the front end, reverse engineering, and memory manipulation. That lets me get a feel for other subjects, and I really like that feeling. of course, it kinda distracts me from reaching the goal that I set for myself, and I see it as a good thing, it helps me keep my interest in computers and not to burn out.',
+        img: 'Me'
+    },
+    {
+        text: 'I don\'t like focusing on things that don\'t make me happy or fulfilled in life. That\'s why I don\'t put all my energy into just one thing. After long days of programming, I love to chill out by doing 3D modeling in Blender. It\'s a great way to unleash creativity and have some fun. There\'s something so enjoyable about letting your imagination run wild. I\'d say I\'m pretty confident when it comes to 3D arts. I don\'t have a ton of experience in 3D modeling, apart from creating scenes, but I do have lots of experience fixing models for 3D printing, planning house layouts, and scene composition. And how could we talk about art without mentioning music? I also picked up playing the guitar as a second hobby. I mainly play the electric bass and electric guitar. Making music allows me to express myself freely and, most importantly, connect with others. I find it easier to bond with people over music than computer engineering because not everyone is a computer geek, so I see it as my way of socializing. With music, I\'ve been able to make friends and connect with other musicians. It\'s something I rely on for many things.',
+        img: '3D'
+    },
+    {
+        text: 'So one of the programmer\'s requirements is to daily touch grass, and for that reason, I love going outside to capture the vibrant city life, the architecture, and the beauty of nature through my photography. Recently, a friend introduced me to Pokemon GO, and now I find myself going on frequent walks to not only catch Pokemon but also to clear my mind. I also use the photos I take as textures and inspiration for the 3D models and software that I work on. It\'s a truly fulfilling and refreshing experience for me.',
+        img: 'Sky'
+    }
 ]
 </script>
 
@@ -29,7 +35,7 @@ const img = [
     </div>
 
     <div name="frames" className="flex flex-col items-center justify-center mt-48">
-        <TextAndImiges text="text" imgSrc="Me" />
+        <TextAndImiges :page="page[0]" indx/>
     </div>
 
 </template>
@@ -48,6 +54,6 @@ color {
 </style>
 <script>
 export default {
-    components: { TextAndImiges }
+    components: { TextAndImiges, TextAndImigesReverst }
 }
 </script>
--- src/components/TextAndImiges.vue
+++ src/components/TextAndImiges.vue
@@ -2,14 +2,15 @@
     <div name="ProgrammingPart" className="flex justify-center items-center gap-56 self-stretch text-center">
         <p
             className="w-6/12 bg-gradient-to-r from-neutral-800 to-neutral-900 p-8 rounded-tr-4xl rounded-bl-4xl border border-neutral-700">
-            
+            {{ page.text }}
         </p>
-        <img :src="`/src/img/${ imgSrc }.jpg`" alt="images of me" className="w-4/12 rounded-4xl border border-neutral-700">
+        <img :src="`/src/img/${page.img}.jpg`" alt="images of me"
+            className="w-4/12 rounded-4xl border border-neutral-700">
     </div>
 </template>
 <script>
-export default{
-    props: [' text ','imgSrc']
+export default {
+    props: ['page']
 }
 </script>
 <style scoped></style>
