8c254f15f73518eba01eef96cea0baa29164450c
JustPause <iamjuststan@hotmail.com>
2024-08-16 04:17:13 +0300

    Jap+

--- src/components/Index.vue
+++ src/components/Index.vue
@@ -5,15 +5,18 @@ import MyWorkSlides from "./MyWorkSlides.vue"
 const pages = [
     {
         text: 'The process of developing software is both time-consuming and incredibly rewarding for me. It involves translating my ideas and concepts into functional fings, you can\'t grasp it but you can feel it. In general, software can significantly influence people\'s lives and my future programs will be capable of affecting others. The process of software development requires careful attention to detail and a strong enthusiasm for solving problems, and this career path enables me to continually learn and adjust to new technologies. I found my path in low-level languages, where the power is in your hands, I like to feel like I can do something special, something that other people can\'t do. I like to expand my knowledge to other topics, from DevOps to the front end, reverse engineering, and memory manipulation. That lets me get a feel for other subjects, and I really like that feeling. of course, it kinda distracts me from reaching the goal that I set for myself, and I see it as a good thing, it helps me keep my interest in computers and not to burn out.',
-        img: 'Me'
+        img: 'Me',
+        altImgText: "Imige of me using the compiuter"
     },
     {
         text: 'I don\'t like focusing on things that don\'t make me happy or fulfilled in life. That\'s why I don\'t put all my energy into just one thing. After long days of programming, I love to chill out by doing 3D modeling in Blender. It\'s a great way to unleash creativity and have some fun. There\'s something so enjoyable about letting your imagination run wild. I\'d say I\'m pretty confident when it comes to 3D arts. I don\'t have a ton of experience in 3D modeling, apart from creating scenes, but I do have lots of experience fixing models for 3D printing, planning house layouts, and scene composition. And how could we talk about art without mentioning music? I also picked up playing the guitar as a second hobby. I mainly play the electric bass and electric guitar. Making music allows me to express myself freely and, most importantly, connect with others. I find it easier to bond with people over music than computer engineering because not everyone is a computer geek, so I see it as my way of socializing. With music, I\'ve been able to make friends and connect with other musicians. It\'s something I rely on for many things.',
-        img: '3D'
+        img: '3D', 
+        altImgText: "Imige of my made 3D model, and renderd in Blender"
     },
     {
         text: 'So one of the programmer\'s requirements is to daily touch grass, and for that reason, I love going outside to capture the vibrant city life, the architecture, and the beauty of nature through my photography. Recently, a friend introduced me to Pokemon GO, and now I find myself going on frequent walks to not only catch Pokemon but also to clear my mind. I also use the photos I take as textures and inspiration for the 3D models and software that I work on. It\'s a truly fulfilling and refreshing experience for me.',
-        img: 'Sky'
+        img: 'Sky', 
+        altImgText: "Imige my made picture of sky"
     }
 ]
 </script>
@@ -39,7 +42,7 @@ const pages = [
     </div>
 
     <div name="frameWork" className="flex flex-col items-end">
-        <MyWorkSlides/>
+        <MyWorkSlides />
     </div>
 
 </template>
--- src/components/MyWorkSlides.vue
+++ src/components/MyWorkSlides.vue
@@ -1,5 +1,51 @@
+<script setup>
+const pages = [{
+    Title: "T",
+    Text: "T",
+    Link: "T"
+}, {
+    Title: "B",
+    Text: "B",
+    Link: "B"
+}, {
+    Title: "C",
+    Text: "C",
+    Link: "C"
+}, {
+    Title: "T",
+    Text: "T",
+    Link: "T"
+}, {
+    Title: "B",
+    Text: "B",
+    Link: "B"
+}, {
+    Title: "C",
+    Text: "C",
+    Link: "C"
+}]
+</script>
+
 <template>
-    <p className="text-5xl font-normal leading-tight text-center mb-4 ">
+    <a className="text-5xl font-normal leading-tight text-center mb-16 " href="https://github.com/JustPause">
         Work
-    </p>
+    </a>
+    <div class="scroll-horizontal">
+        <div class="contaner">
+            <img src="/src/img/3D.jpg" alt="" srcset="">
+            <img src="/src/img/3D.jpg" alt="" srcset="">
+            <img src="/src/img/3D.jpg" alt="" srcset="">
+        </div>
+        <!-- <a class="contaner" href="#">
+            <h1>Header</h1>
+            <p>text,text,text,text,text,text,text,text,text,text,text,text,text,text,text,text,text,text,text,text,text,text,text,text,text,text,text,text,text
+            </p>
+        </a> -->
+
+       < https://codepen.io/kevinpowell/pen/BavVLra>
+    </div>
 </template>
+
+<style scoped>
+
+</style>
