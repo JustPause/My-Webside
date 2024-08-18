7b2daf4b6714f54833790ad63b816ff8521b6dfc
JustPause <Justinaslolt@gmail.com>
2024-08-19 01:37:38 +0300

    Proges

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,8 @@
 import Header from "./components/Header.vue";
 import Footer from "./components/Footer.vue";
 
-import mainWindow from "./components/Index.vue"
+import mainWindow from "./components/Index.vue";
+
 </script>
 
 <template>
--- src/components/MyWorkSlides.vue
+++ src/components/MyWorkSlides.vue
@@ -1,9 +1,9 @@
 <script setup>
 const pages = [
   {
-    Title: "T",
-    Text: "T",
-    Link: "T",
+    Title: "My andriod project",
+    Text: "In Vilnius Coding Scool, my final project was, to make a project in Java with SQL. So that is my project. It is the Andriod program, main in Andriod studio, with SQLite database. With Room framework.",
+    Link: "#",
   },
   {
     Title: "B",
@@ -46,79 +46,14 @@ const pages = [
     <a
       class="contaner"
       className="w-72 bg-slate-600 p-8 rounded-4xl border border-neutral-700 ml-4"
-      href="#"
+      href={{page.Link}} v-for="page in pages"
     >
-      <h1 className="text-2xl pb-2">My andriod project</h1>
+      <h1 className="text-2xl pb-2">{{page.Title}}</h1>
       <p className="overflow-ellipsis break-words text-justify">
-        In Vilnius Coding Scool, my final project was, to make a project in Java
-        with SQL. So that is my project. It is the Andriod program, main in
-        Andriod studio, with SQLite database. With Room framework.
+        {{page.Text}}
       </p>
     </a>
-    <a
-      class="contaner"
-      className="w-72 bg-slate-600 p-8 rounded-4xl border border-neutral-700 ml-4"
-      href="#"
-    >
-      <h1 className="text-2xl pb-2">My andriod project</h1>
-      <p className="overflow-ellipsis break-words text-justify">
-        In Vilnius Coding Scool, my final project was, to make a project in Java
-        with SQL. So that is my project. It is the Andriod program, main in
-        Andriod studio, with SQLite database. With Room framework.
-      </p>
-    </a>
-    <a
-      class="contaner"
-      className="w-72 bg-slate-600 p-8 rounded-4xl border border-neutral-700 ml-4"
-      href="#"
-    >
-      <h1 className="text-2xl pb-2">My andriod project</h1>
-      <p className="overflow-ellipsis break-words text-justify">
-        In Vilnius Coding Scool, my final project was, to make a project in Java
-        with SQL. So that is my project. It is the Andriod program, main in
-        Andriod studio, with SQLite database. With Room framework.
-      </p>
-    </a>
-
-    <a
-      class="contaner"
-      className="w-72 bg-slate-600 p-8 rounded-4xl border border-neutral-700 ml-4"
-      href="#"
-    >
-      <h1 className="text-2xl pb-2">My andriod project</h1>
-      <p className="overflow-ellipsis break-words text-justify">
-        In Vilnius Coding Scool, my final project was, to make a project in Java
-        with SQL. So that is my project. It is the Andriod program, main in
-        Andriod studio, with SQLite database. With Room framework.
-      </p>
-    </a>
-    <a
-      class="contaner"
-      className="w-72 bg-slate-600 p-8 rounded-4xl border border-neutral-700 ml-4"
-      href="#"
-    >
-      <h1 className="text-2xl pb-2">My andriod project</h1>
-      <p className="overflow-ellipsis break-words text-justify">
-        In Vilnius Coding Scool, my final project was, to make a project in Java
-        with SQL. So that is my project. It is the Andriod program, main in
-        Andriod studio, with SQLite database. With Room framework.
-      </p>
-    </a>
-    <a
-      class="contaner"
-      className="w-72 bg-slate-600 p-8 rounded-4xl border border-neutral-700 ml-4"
-      href="#"
-    >
-      <h1 className="text-2xl pb-2">My andriod project</h1>
-      <p className="overflow-ellipsis break-words text-justify">
-        In Vilnius Coding Scool, my final project was, to make a project in Java
-        with SQL. So that is my project. It is the Andriod program, main in
-        Andriod studio, with SQLite database. With Room framework.
-      </p>
-    </a>
-
+    
     <!-- https://codepen.io/kevinpowell/pen/BavVLra -->
   </div>
 </template>
-
-<style scoped></style>
